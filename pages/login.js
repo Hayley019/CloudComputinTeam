1e893e45ef7f6fc5eb6bb8b45667eba221878331
= <=>
2024-03-17 19:04:28 -0600

    Correcciones para vercel

--- pages/login.js
+++ pages/login.js
@@ -1,17 +1,8 @@
-import Google from 'next-auth/providers/google';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
-import { useRouter } from 'next/router';
+
 
 
 function loginPage() {
-
-    const {data: session, status} = useSession()
-    const router = useRouter()
-
-    if(status !== "loading" && status === "authenticated"){
-        router.push("/")
-    }
-
   return (
     <div>
       <button onClick={() => signIn()}>Iniciar sesión</button>
