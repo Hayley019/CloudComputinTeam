fc78d603429d07f0e8775984448f386b0c20dc13
PabloPizarro8 <l20550431@chihuahua2.tecnm.mx>
2024-05-28 22:18:56 -0600

    si

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import { MongoClient } from "mongodb";
 import { MongoDBAdapter } from '@auth/mongodb-adapter';
 import clientPromise from "@lib/mongodb";
@@ -42,6 +43,47 @@ export const authOptions = {
           // Si la contraseña no coincide, denegamos el acceso
           return null;
         }
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      
+      // save profile to database, adding the role
+      async profile(profile) {
+        console.log("Google");
+        const client = new MongoClient(process.env.MONGODB_URI);
+        await client.connect();
+
+        const collection = client.db("Tour").collection("Users");
+        const existingUser = await collection.findOne({ email: profile.email });
+
+        if (existingUser) {
+          // Si el usuario ya existe
+          console.log("User already exists");
+
+          
+        } else {
+          // Si el usuario no existe, lo registramos
+          const newUser = {
+            email: profile.email,
+            username: profile.name, // Puedes ajustar esto según la información que desees almacenar
+            
+            provider: "Google",
+            // Aquí puedes almacenar más información según la que proporciona el objeto 'profile'
+            role: "user"
+          };
+
+          await collection.insertOne(newUser);
+          
+        }
+
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+        };
+
       },
     }),
     GithubProvider({
